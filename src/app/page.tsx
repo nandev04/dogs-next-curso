9887657ecbdbf0e6aca3cb4ff1da2bd5688ca1d4
Renan Alves <renanlvsdv@gmail.com>
2025-03-27 08:59:12 -0300

    feat: importando fontes com next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <h1>home</h1>
+      <h1 className="title">Dogs Next</h1>
     </main>
   );
 }
